018f78c72ccaaca374ae1ae4d994b18622a9d4f9
Hummie <humphreywill77@gmail.com>
2023-06-15 05:21:49 -0700

    Size of variable

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+  *main - starting point
+  *
+  *Description: printf is outputting the size of variables
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	printf("size of char: %zu bytes\n", size of(char));
+	printf("size of float: %zu bytes\n", size of(float));
+	printf("size of int: %zu bytes\n", size of(int));
+	printf("size of long int: %zu bytes\n", size of(long int));
+	printf("size of long long int: %zu bytes\n", size of(long long int));
+
+	return (0);
+}
