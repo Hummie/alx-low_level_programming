84bc213505b64661702a5f7be5208084f607f0b7
Hummie <humphreywill77@gmail.com>
2023-06-15 04:28:57 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+  *main - starting function
+  *
+  *Description: printf outputs the statement.
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
