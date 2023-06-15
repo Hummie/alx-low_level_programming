1670bb0aa0b7857a52b1bedd8ed630f44f623e7a
Hummie <humphreywill77@gmail.com>
2023-06-15 04:00:36 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
-include < stdio.h >
-int main(void);
+#include < stdio.h >
+
+/**
+  *Main-starting point
+  *
+  *Description: The puts function outputs the sentence
+  *
+  *Return 0
+  */
+int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
 
