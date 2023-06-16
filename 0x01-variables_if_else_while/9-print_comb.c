026a74e0ff9c28012d88046e7e4c46f70b9fc1d9
Hummie <humphreywill77@gmail.com>
2023-06-16 07:06:43 -0700

    Printing all possible combinations of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  *main - staring function
+  *
+  *Description: printing single digit numbers and adding a comma and space in between.
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		putchar (i + '0');
+		if (i != 9)
+		{
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
