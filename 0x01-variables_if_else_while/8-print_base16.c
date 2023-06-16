f18f078f8b08ee2ffccbf4beedbaf8c6af5f7687
Hummie <humphreywill77@gmail.com>
2023-06-16 06:43:21 -0700

    Printing base 16 numbers starting from 0

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  *main - staring function
+  *
+  *Description: printing hexadecimal numbers.
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	int f;
+	char let;
+
+	for (f = '0'; f <= 9; f++)
+	putchar(f);
+
+	for (let = 'a'; let <= 'f'; let++)
+	putchar(let);
+	putchar('\n');
+	return (0);
+}
