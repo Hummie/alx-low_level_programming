68d0b99f82d4a8aefb4825935955764ff1968a77
Hummie <humphreywill77@gmail.com>
2023-06-16 11:58:34 -0700

    Possible combination of two two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,22 @@
   */
 int main(void)
 {
-	int i, j, k, l;
+	int i, j;
 
 	for (i = 0; i < 100; i++)
 	{
 		for (j = i + 1; j < 100; j++)
 		{
-			for (k = j + 1; k < 100; k++)
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (i < 99 || j < 99)
 			{
-				for (l = k + 1; l < 100; l++)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-					putchar(l + '0');
-
-					if (i < 96 || j < 97 || k < 98 || l < 99)
-					{
-
 				putchar(',');
 				putchar(' ');
-					}
-				}
 			}
 		}
 	}
