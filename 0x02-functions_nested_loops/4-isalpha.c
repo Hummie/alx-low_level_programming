09e9659a14e369125f99f22e4d5e6bf6b1edd7a9
Hummie <humphreywill77@gmail.com>
2023-06-20 04:54:51 -0700

    Letter check

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  *_isalpha - checking for a letter
+  *
+  *@c: parameter to be assessed
+  *
+  *Return: 1 for lowercase and 0 if not
+  *
+  */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		if (c >= 'A' && c <= 'Z')
+		{
+		return (1);
+		}
+		return (0);
+	return (1);
+	}
+	return (0);
+}
+
