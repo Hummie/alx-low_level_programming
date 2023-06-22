3f17c8b63eb13514d8741f4464a6a7842f210ff6
Hummie <humphreywill77@gmail.com>
2023-06-22 00:50:42 -0700

    Checking for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *_isupper - starting function
+  *@c: check the uppercase
+  *Return: 1 0r 0
+  */
+
+int _isupper(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (0);
+	}
+	else
+	{
+	return (1);
+	}
+	_putchar('\n');
+}
