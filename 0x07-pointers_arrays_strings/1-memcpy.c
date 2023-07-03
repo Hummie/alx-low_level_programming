2df9137ae511c290faecc555da238c4323cb1f00
Hummie <humphreywill77@gmail.com>
2023-07-03 01:03:42 -0700

    Coping memory area of n bytes

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *_memcpy - function to copy memory area
+  *@dest: memeory area to be copied into
+  *@src: memory area to be copied from
+  *Return: dest
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0 ; i++)
+	{
+		dest[i] = src[i];
+		n--;
+	}
+	return (dest);
+}
