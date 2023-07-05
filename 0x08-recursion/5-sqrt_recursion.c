96d42b670e0dfb3a202256f5cd98a11223301f82
Hummie <humphreywill77@gmail.com>
2023-07-05 06:27:52 -0700

    Square root function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  *sqrt - function to find square root of a number
+  *sqrtact - function to find square root of a number
   *@a: variable to find square root
   *@b: iterates through a
   *Return: 1 or -1
@@ -11,7 +11,7 @@ int sqrtact(int a, int b)
 		return (b);
 	if (b * b > a)
 		return (-1);
-	return sqrtact(a, b + 1);
+	return (sqrtact(a, b + 1));
 }
 
 #include "main.h"
