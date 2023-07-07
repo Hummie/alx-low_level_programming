44dc415c1c700afbb0ac97b747de7a8825d21fb4
Hummie <humphreywill77@gmail.com>
2023-07-07 05:55:20 -0700

    Program printing name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,11 +7,6 @@
   */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("argv[%d] = %s\n", i, argv[i]);
-	}
+	printf("%s\n", argv[argc - argc]);
 	return (0);
 }
