98b65fc4bc04a195a6dacae36a6467082ff702e2
Hummie <humphreywill77@gmail.com>
2023-07-07 06:49:06 -0700

    Multiplication of commands

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *main - starting function
+  *@argc: counter of string argv
+  *@argv: string
+  *Return: 1 if error 0 for success
+  */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc < 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi( argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}
