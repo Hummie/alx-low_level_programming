c90c71b5a176b7a4bddf00393a97261565d8da35
Hummie <humphreywill77@gmail.com>
2023-07-13 04:48:06 -0700

    Concantenate string with unknown length n

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,13 +11,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int j;
 	char *ncon;
-	int i, ncon_str = 0, len = 0;
+	int i, ncon_str = 0, len = 1;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i  = 0, j = 0; s1[i] || s2[j]; i++, j++)
+	for (i  = 0, j = 0; s1[i] != '\0' || s2[j] != '\0'; i++, j++)
 		len++;
 
 	ncon = malloc(sizeof(char) * (len + 1));
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i]; i++)
 		ncon[ncon_str++] = s1[i];
 
-	for (j = 0; s2[j] && j < n; j++)
+	for (j = 0; s2[j] != '\0' && j < n; j++)
 		ncon[ncon_str++] = s2[j];
 
 	ncon[ncon_str] = '\0';
