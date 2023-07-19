bba367e6bad0331eb98a3ef74b260a70ea72c41d
Hummie <humphreywill77@gmail.com>
2023-07-19 03:58:49 -0700

    Printing name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+  *print_name - function to print name
+  *@name: string
+  *@f: pointer to function
+  *Return: nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
