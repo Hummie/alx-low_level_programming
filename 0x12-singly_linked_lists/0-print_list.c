a6bc9f8c49157a502e2f48fa9b667bd035f0b178
Hummie <humphreywill77@gmail.com>
2023-07-27 05:31:42 -0700

    Printing elements of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  *print_list - function that prints elements of a list
+  *@h: pointer to node structure
+  *
+  *Return: Number of nodes
+  */
+size_t print_list(const list_t *h)
+{
+	unsigned int num_node;
+
+	num_node = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		num_node++;
+	}
+	return (num_node);
+}
