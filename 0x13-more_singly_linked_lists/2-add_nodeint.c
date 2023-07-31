2f7600e8911b66f5e4949fbe30dcdc4ca735f015
Hummie <humphreywill77@gmail.com>
2023-07-31 10:35:32 -0700

    Adiing a new node at the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,7 +10,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newnode = malloc(sizeof(listint_t));
 
-	newnode->n = n;
 
 	if (newnode == NULL)
 	{
@@ -18,6 +17,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	}
 	else
 	{
+		newnode->n = n;
 		newnode->next = *head;
 		*head = newnode;
 	}
