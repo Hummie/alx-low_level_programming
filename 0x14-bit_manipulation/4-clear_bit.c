d96f1821a334653b17f3078faacdcb14ab0bc915
Hummie <humphreywill77@gmail.com>
2023-08-03 12:43:57 -0700

    Set value at index to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *clear_bit - function to set value at index to 0
+  *@n: string with bits
+  *@index: position of bit in string
+  @Return: 1 or -1
+  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	mask = 1UL << index;
+	
+	mask = ~mask;
+	*n &= mask;
+
+	return (1);
+}
+
