5f142d287fdab0ebdcc8f5cd929561f6f89f7d47
Hummie <humphreywill77@gmail.com>
2023-08-03 12:11:40 -0700

    Getting binary at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,8 +13,8 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	val = n >> 1;
-	bit = val & 1;
+	val = n >> index;
+	bit = val & 0x1;
 
 	return (bit);
 }
